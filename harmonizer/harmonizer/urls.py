a6464e09cb5774dc76352f89ba5a858256f6b47e
Łukasz Kazimierczak <lukasz.kazimierczak@comarch.pl>
2015-12-15 12:27:09 +0100

    Add first application and views

--- harmonizer/harmonizer/urls.py
+++ harmonizer/harmonizer/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'harmonizer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^witam/', include('Witam.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
